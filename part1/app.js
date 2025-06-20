a4e7c677ea934812a8a37fadf3ddc3f32c61e6fd
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-20 12:07:40 +0000

    [automatic commit] 2025-06-20 12:07:40 UTC 122.108.17.129

--- part1/app.js
+++ part1/app.js
@@ -19,7 +19,8 @@ app.get('/api/dogs', async (req, res) => {
     try {
         const [rows] = await pool.query(
             `SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            FROM Dogs d`
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id;`
         )
     }
 });
