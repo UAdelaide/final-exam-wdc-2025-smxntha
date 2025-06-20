ff7a9e66c57f3d58265ce854c51ba84378626008
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-20 12:06:38 +0000

    [automatic commit] 2025-06-20 12:06:38 UTC 122.108.17.129

--- part1/app.js
+++ part1/app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 // GET: List of all dogs (name and size_ and ownder username)
 app.get('/api/dogs', async (req, res) => {
     try {
-        const [rows] = await pool.query
+        const [rows] = await pool.query(
+            `SELECT `
+        )
     }
 });
