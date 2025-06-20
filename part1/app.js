edcceee051311f84c924c55f2c47dafd89f80beb
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-20 12:24:18 +0000

    [automatic commit] 2025-06-20 12:24:18 UTC 122.108.17.129

--- part1/app.js
+++ part1/app.js
@@ -39,8 +39,8 @@ app.get('/api/walkrequests/open', async (req, res) {
             JOIN Users u ON d.owner_id = u.user_id
             WHERE wr.status = 'open';`
         );
-        res.json(openReqRows)
+        res.json(openReqRows);
     } catch (err) {
-
+        res.status(500).json({ 'Failed to fetch open '})
     }
 });
