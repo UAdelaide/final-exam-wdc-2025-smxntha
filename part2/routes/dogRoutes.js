b11e426b88483aa1c40c4fe258e1f00453df9c5c
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-21 03:25:00 +0000

    [automatic commit] 2025-06-21 03:25:00 UTC 122.108.17.129

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,6 @@ const db = require('../models/db');
 // GET: Dogs owned by the logged in user
 router.get('/owned', async (req, res) => {
     const ownerId = req.session.userID;
-    console.log('ownerId:', req.session.userID);
 
     if (!ownerId) {
         return res.status(401).json({ error: 'Unauthorized user.' });
