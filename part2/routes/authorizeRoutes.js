320b968c983c18bfa0c48f3529468a41a8d4828d
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-21 02:01:12 +0000

    [automatic commit] 2025-06-21 02:01:11 UTC 122.108.17.129

--- part2/routes/authorizeRoutes.js
+++ part2/routes/authorizeRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const bcrypt = require('bcrypt'); // for password hashing
 
 // POST: login
 router.post('/login', async (req, res) => {
