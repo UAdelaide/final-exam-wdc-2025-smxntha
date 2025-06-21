d8dba30999bf2cecff8f3c272090f654b70e098e
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-21 03:12:45 +0000

    [automatic commit] 2025-06-21 03:12:45 UTC 122.108.17.129

--- part2/app.js
+++ part2/app.js
@@ -25,8 +25,8 @@ const dogRoutes = require('./routes/dogRoutes'); // add route for user dogs
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/auth', authRoutes); // mount for logins
-app.use('/api/dogs', dogRoutes); // 
+app.use('/auth', authRoutes); // for login authorisation
+app.use('/api/dogs', dogRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
