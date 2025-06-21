a1edba2c490cc6b04b2ca962e3d02139bcb5260a
WDC Student (missing default git user name) <a1851705@adelaide.edu.au>
2025-06-21 01:50:14 +0000

    [automatic commit] 2025-06-21 01:50:13 UTC 122.108.17.129

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,7 @@ const authRoutes = require('./routes/authorizeRoutes'); // add route for authori
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-
+app.use('/login')
 
 // Export the app instead of listening here
 module.exports = app;
